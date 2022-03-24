d5b13d268296b46a981cbd0128288882e92bf459
shivampandeyvg2 <shivampandeyvg@gmail.com>
2022-03-25 00:26:11 +0530

    Heroku deployment

--- OrdersApp/settings.py
+++ OrdersApp/settings.py
@@ -131,6 +131,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+# STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 django_heroku.settings(locals())
