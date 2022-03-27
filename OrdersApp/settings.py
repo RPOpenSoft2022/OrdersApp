712e69dd3fcb094a204999fe63f17b7c56391cef
Vaibhav Mohite <vaibhavdmohite22@gmail.com>
2022-03-28 01:16:31 +0530

    Added corsheaders

--- OrdersApp/settings.py
+++ OrdersApp/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'Orders_App'
+    'Orders_App',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +60,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'OrdersApp.urls'
