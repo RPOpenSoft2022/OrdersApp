fd3f257d4777234ab28dd43b61832167d885f03e
TheThinker01 <archisman.pathak@iitkgp.ac.in>
2022-03-31 01:41:19 +0530

    updated allowed hosts

--- OrdersApp/settings.py
+++ OrdersApp/settings.py
@@ -77,6 +77,7 @@ CORS_ALLOW_METHODS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://anytimefood.azurewebsites.net/"
 ]
 
 CORS_ALLOW_CREDENTIALS=True
